b05d325c7e08fc88c81225fde69313fce67c0b62
Imad Bourouche <ji_bourouche@esi.dz>
2025-05-07 16:39:21 +0200

    Change configuration Ontoportal-Astro API (#2)
    * change configuration to meet the v4 requirements
    
    * add env path to use bundle
    
    * change configurations to ontoportal-astro config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-set :author, "ontoportal-lirmm"
+set :author, "ontoportal-astro"
 set :application, "ontologies_api"
 set :repo_url, "https://github.com/#{fetch(:author)}/#{fetch(:application)}.git"
 
@@ -8,7 +8,7 @@ set :deploy_via, :remote_cache
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, "/srv/ontoportal/#{fetch(:application)}"
+set :deploy_to, "/opt/ontoportal/ontologies_api"
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -23,7 +23,7 @@ set :log_level, :error
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+append :linked_files, 'config/unicorn.rb', 'config/environments/appliance.rb', 'config/environments/site_config.rb'
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -31,38 +31,29 @@ set :linked_dirs, %w{log vendor/bundle tmp/pids tmp/sockets public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
+set :default_env, {
+  'PATH' => "/usr/local/rbenv/shims:/usr/local/rbenv/bin:/usr/bin:$PATH"
+}
 # Default value for keep_releases is 5
 set :keep_releases, 5
 set :config_folder_path, "#{fetch(:application)}/#{fetch(:stage)}"
 
 # If you want to restart using `touch tmp/restart.txt`, add this to your config/deploy.rb:
 
-SSH_JUMPHOST = ENV.include?('SSH_JUMPHOST') ? ENV['SSH_JUMPHOST'] : 'jumpbox.hostname.com'
-SSH_JUMPHOST_USER = ENV.include?('SSH_JUMPHOST_USER') ? ENV['SSH_JUMPHOST_USER'] : 'username'
+# SSH_JUMPHOST = ENV.include?('SSH_JUMPHOST') ? ENV['SSH_JUMPHOST'] : 'jumpbox.hostname.com'
+# SSH_JUMPHOST_USER = ENV.include?('SSH_JUMPHOST_USER') ? ENV['SSH_JUMPHOST_USER'] : 'username'
+# JUMPBOX_PROXY = "#{SSH_JUMPHOST_USER}@#{SSH_JUMPHOST}"
 
-JUMPBOX_PROXY = "#{SSH_JUMPHOST_USER}@#{SSH_JUMPHOST}"
 set :ssh_options, {
   user: 'ontoportal',
-  forward_agent: 'true',
-  keys: %w(config/deploy_id_rsa),
-  auth_methods: %w(publickey),
-  # use ssh proxy if API servers are on a private network
-  proxy: Net::SSH::Proxy::Command.new("ssh #{JUMPBOX_PROXY} -W %h:%p")
+  # keys: %w(config/deploy_id_rsa),
+  # auth_methods: %w(publickey),
+  # forward_agent: 'true',
+  # proxy: Net::SSH::Proxy::Command.new("ssh #{JUMPBOX_PROXY} -W %h:%p")
 }
 
 # private git repo for configuraiton
-PRIVATE_CONFIG_REPO = ENV.include?('PRIVATE_CONFIG_REPO') ? ENV['PRIVATE_CONFIG_REPO'] : 'https://your_github_pat_token@github.com/your_organization/ontoportal-configs.git'
-desc "Check if agent forwarding is working"
-task :forwarding do
-  on roles(:all) do |h|
-    if test("env | grep SSH_AUTH_SOCK")
-      info "Agent forwarding is up to #{h}"
-    else
-      error "Agent forwarding is NOT up to #{h}"
-    end
-  end
-end
+PRIVATE_CONFIG_REPO = ENV.include?('PRIVATE_CONFIG_REPO') ? ENV['PRIVATE_CONFIG_REPO'] : "https://your_github_pat_token@github.com/#{fetch(:author)}/ontoportal-configs.git"
 
 # inspired by http://nathaniel.talbott.ws/blog/2013/03/14/post-deploy-smoke-tests/
 desc 'Run smoke test'
@@ -92,43 +83,40 @@ end
 
 namespace :deploy do
 
-  desc 'Incorporate the private repository content'
-  # Get cofiguration from repo if PRIVATE_CONFIG_REPO env var is set
-  # or get config from local directory if LOCAL_CONFIG_PATH env var is set
-  task :get_config do
-    if defined?(PRIVATE_CONFIG_REPO)
-      TMP_CONFIG_PATH = "/tmp/#{SecureRandom.hex(15)}".freeze
-      on roles(:app) do
-        execute "git clone -q #{PRIVATE_CONFIG_REPO} #{TMP_CONFIG_PATH}"
-        execute "rsync -av #{TMP_CONFIG_PATH}/#{fetch(:config_folder_path)}/ #{release_path}/"
-        execute "rm -rf #{TMP_CONFIG_PATH}"
-      end
-    elsif defined?(LOCAL_CONFIG_PATH)
-      on roles(:app) do
-        execute "rsync -av #{LOCAL_CONFIG_PATH}/#{fetch(:application)}/ #{release_path}/"
-      end
-    end
-  end
+  # desc 'Incorporate the private repository content'
+  # # Get cofiguration from repo if PRIVATE_CONFIG_REPO env var is set
+  # # or get config from local directory if LOCAL_CONFIG_PATH env var is set
+  # task :get_config do
+  #   if defined?(PRIVATE_CONFIG_REPO)
+  #     TMP_CONFIG_PATH = "/tmp/#{SecureRandom.hex(15)}".freeze
+  #     on roles(:app) do
+  #       execute "git clone -q #{PRIVATE_CONFIG_REPO} #{TMP_CONFIG_PATH}"
+  #       execute "rsync -av #{TMP_CONFIG_PATH}/#{fetch(:config_folder_path)}/ #{release_path}/"
+  #       execute "rm -rf #{TMP_CONFIG_PATH}"
+  #     end
+  #   elsif defined?(LOCAL_CONFIG_PATH)
+  #     on roles(:app) do
+  #       execute "rsync -av #{LOCAL_CONFIG_PATH}/#{fetch(:application)}/ #{release_path}/"
+  #     end
+  #   end
+  # end
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      execute 'sudo systemctl restart unicorn'
-      execute 'sleep 5'
+      execute 'sudo systemctl restart unicorn.service'
     end
   end
 
-  after :updating, :get_config
+  # after :updating, :get_config
   after :publishing, :restart
+  after :restart, :clear_cache 
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  task :clear_cache do
+    on roles(:app), in: :sequence, wait: 5 do
+        execute 'sudo opclearcaches'
     end
   end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,8 +4,8 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w[agroportal.lirmm.fr]
-role :db, %w[agroportal.lirmm.fr] # sufficient to run db:migrate only on one system
+role :app, %w{ontoportal-astro.eu}
+role :db, %w{ontoportal-astro.eu} # sufficient to run db:migrate only on one system
 set :branch, ENV.include?('BRANCH') ? ENV['BRANCH'] : 'master'
 # Extended Server Syntax
 # ======================
@@ -13,5 +13,5 @@ set :branch, ENV.include?('BRANCH') ? ENV['BRANCH'] : 'master'
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+#server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 set :log_level, :error
